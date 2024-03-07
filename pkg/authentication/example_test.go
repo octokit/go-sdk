46e0476b48352a68e97ba6eebe1d4e330f69be95
Keegan Campbell <me@kfcampbell.com>
2024-03-07 13:26:39 -0800

    More progress on rate limiting using Kiota handlers

--- pkg/authentication/example_test.go
+++ pkg/authentication/example_test.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
-	"time"
 
 	abstractions "github.com/microsoft/kiota-abstractions-go"
 	kiotaHttp "github.com/microsoft/kiota-http-go"
@@ -13,7 +11,6 @@ import (
 	"github.com/octokit/go-sdk/pkg/github"
 	"github.com/octokit/go-sdk/pkg/github/octocat"
 	"github.com/octokit/go-sdk/pkg/handlers"
-	"github.com/octokit/go-sdk/pkg/transport"
 )
 
 // ExampleApiClient_Octocat shows how to initialize an unauthenticated client
@@ -74,149 +71,6 @@ func ExampleApiClient_Octocat() {
 	//                  :~==~==~==~==~~
 }
 
-func Example_ResponseHandlerForRateLimiting() {
-	// this responseHandler function panics:
-	// panic: interface conversion: *http.Response is not serialization.Parsable: missing method GetFieldDeserializers
-	// not sure if i'm mishandling something in this totally non-implemented function or what
-	responseHandler := func(response interface{}, errorMappings abstractions.ErrorMappings) (interface{}, error) {
-		// handle rate-limited responses here
-		fmt.Printf("%v\n", response)
-		if response == nil {
-			return nil, nil
-		}
-		return response, nil
-	}
-	responseHandlerOption := abstractions.NewRequestHandlerOption()
-	responseHandlerOption.SetResponseHandler(responseHandler)
-
-	options := make([]abstractions.RequestOption, 0)
-	options = append(options, responseHandlerOption)
-
-	retryHandlerOption := &kiotaHttp.RetryHandlerOptions{
-		MaxRetries: 3,
-	}
-	options = append(options, retryHandlerOption)
-
-	tokenProvider := auth.NewTokenProvider(
-		// to create an authenticated provider, uncomment the below line and pass in your token
-		// auth.WithAuthorizationToken("ghp_your_token"),
-		auth.WithUserAgent("octokit/go-sdk.example-functions"),
-	)
-
-	adapter, err := kiotaHttp.NewNetHttpRequestAdapter(tokenProvider)
-	if err != nil {
-		log.Fatalf("Error creating request adapter: %v", err)
-	}
-	client := github.NewApiClient(adapter)
-
-	s := "Salutations"
-
-	// create headers that accept json back; GitHub's OpenAPI definition says
-	// octet-stream but that's not actually what the API returns in this case
-	headers := abstractions.NewRequestHeaders()
-	_ = headers.TryAdd("Accept", "application/vnd.github.v3+json")
-
-	octocatRequestConfig := &abstractions.RequestConfiguration[octocat.OctocatRequestBuilderGetQueryParameters]{
-		QueryParameters: &octocat.OctocatRequestBuilderGetQueryParameters{
-			S: &s,
-		},
-		Headers: headers,
-		Options: options,
-	}
-
-	cat, err := client.Octocat().Get(context.Background(), octocatRequestConfig)
-	if err != nil {
-		log.Fatalf("error getting octocat: %v", err)
-	}
-	fmt.Printf("%v\n", string(cat))
-	// Output:
-	// MMM.           .MMM
-	//                MMMMMMMMMMMMMMMMMMM
-	//                MMMMMMMMMMMMMMMMMMM      _____________
-	//               MMMMMMMMMMMMMMMMMMMMM    |             |
-	//              MMMMMMMMMMMMMMMMMMMMMMM   | Salutations |
-	//             MMMMMMMMMMMMMMMMMMMMMMMM   |_   _________|
-	//             MMMM::- -:::::::- -::MMMM    |/
-	//              MM~:~ 00~:::::~ 00~:~MM
-	//         .. MMMMM::.00:::+:::.00::MMMMM ..
-	//               .MM::::: ._. :::::MM.
-	//                  MMMM;:::::;MMMM
-	//           -MM        MMMMMMM
-	//           ^  M+     MMMMMMMMM
-	//               MMMMMMM MM MM MM
-	//                    MM MM MM MM
-	//                    MM MM MM MM
-	//                 .~~MM~MM~MM~MM~~.
-	//              ~~~~MM:~MM~~~MM~:MM~~~~
-	//             ~~~~~~==~==~~~==~==~~~~~~
-	//              ~~~~~~==~==~==~==~~~~~~
-	//                  :~==~==~==~==~~
-}
-
-func Example_HttpClientForRateLimiting() {
-	tokenProvider := auth.NewTokenProvider(
-		// to create an authenticated provider, uncomment the below line and pass in your token
-		// auth.WithAuthorizationToken("ghp_your_token"),
-		auth.WithUserAgent("octokit/go-sdk.example-functions"),
-	)
-
-	transport := transport.NewRateLimitTransport(http.DefaultTransport,
-		transport.WithWriteDelay(1*time.Second),
-		transport.WithReadDelay(0*time.Second),
-		transport.WithParallelRequests(false))
-	httpClient := &http.Client{
-		Transport: transport,
-	}
-
-	adapter, err := kiotaHttp.NewNetHttpRequestAdapterWithParseNodeFactoryAndSerializationWriterFactoryAndHttpClient(tokenProvider, nil, nil, httpClient)
-	if err != nil {
-		log.Fatalf("Error creating request adapter: %v", err)
-	}
-	client := github.NewApiClient(adapter)
-
-	s := "Salutations"
-
-	// create headers that accept json back; GitHub's OpenAPI definition says
-	// octet-stream but that's not actually what the API returns in this case
-	headers := abstractions.NewRequestHeaders()
-	_ = headers.TryAdd("Accept", "application/vnd.github.v3+json")
-
-	octocatRequestConfig := &abstractions.RequestConfiguration[octocat.OctocatRequestBuilderGetQueryParameters]{
-		QueryParameters: &octocat.OctocatRequestBuilderGetQueryParameters{
-			S: &s,
-		},
-		Headers: headers,
-	}
-
-	cat, err := client.Octocat().Get(context.Background(), octocatRequestConfig)
-	if err != nil {
-		log.Fatalf("error getting octocat: %v", err)
-	}
-	fmt.Printf("%v\n", string(cat))
-	// Output:
-	// MMM.           .MMM
-	//                MMMMMMMMMMMMMMMMMMM
-	//                MMMMMMMMMMMMMMMMMMM      _____________
-	//               MMMMMMMMMMMMMMMMMMMMM    |             |
-	//              MMMMMMMMMMMMMMMMMMMMMMM   | Salutations |
-	//             MMMMMMMMMMMMMMMMMMMMMMMM   |_   _________|
-	//             MMMM::- -:::::::- -::MMMM    |/
-	//              MM~:~ 00~:::::~ 00~:~MM
-	//         .. MMMMM::.00:::+:::.00::MMMMM ..
-	//               .MM::::: ._. :::::MM.
-	//                  MMMM;:::::;MMMM
-	//           -MM        MMMMMMM
-	//           ^  M+     MMMMMMMMM
-	//               MMMMMMM MM MM MM
-	//                    MM MM MM MM
-	//                    MM MM MM MM
-	//                 .~~MM~MM~MM~MM~~.
-	//              ~~~~MM:~MM~~~MM~:MM~~~~
-	//             ~~~~~~==~==~~~==~==~~~~~~
-	//              ~~~~~~==~==~==~==~~~~~~
-	//                  :~==~==~==~==~~
-}
-
 func Example_RateLimitHandler() {
 	rateLimitHandler := handlers.NewRateLimitHandler()
 	middlewares := kiotaHttp.GetDefaultMiddlewares()
@@ -229,8 +83,8 @@ func Example_RateLimitHandler() {
 		auth.WithUserAgent("octokit/go-sdk.example-functions"),
 	)
 
+	// TODO(kfcampbell): this constructor is unwieldy. can we make an abstraction to make it cleaner?
 	adapter, err := kiotaHttp.NewNetHttpRequestAdapterWithParseNodeFactoryAndSerializationWriterFactoryAndHttpClient(tokenProvider, nil, nil, netHttpClient)
-	// adapter, err := kiotaHttp.NewNetHttpRequestAdapter(tokenProvider)
 	if err != nil {
 		log.Fatalf("Error creating request adapter: %v", err)
 	}
@@ -248,7 +102,6 @@ func Example_RateLimitHandler() {
 			S: &s,
 		},
 		Headers: headers,
-		// Options: options,
 	}
 
 	cat, err := client.Octocat().Get(context.Background(), octocatRequestConfig)
--- pkg/handlers/rate_limit_handler.go
+++ pkg/handlers/rate_limit_handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	netHttp "net/http"
+	"strconv"
 	"time"
 
 	abs "github.com/microsoft/kiota-abstractions-go"
@@ -15,9 +16,8 @@ type RateLimitHandler struct {
 }
 
 type RateLimitHandlerOptions struct {
-	ReadDelay        time.Duration
-	WriteDelay       time.Duration
-	ParallelRequests bool
+	ReadDelay  time.Duration
+	WriteDelay time.Duration
 }
 
 type RateLimitType int
@@ -51,7 +51,7 @@ func (options *RateLimitHandlerOptions) IsRateLimited() func(req *netHttp.Reques
 		}
 
 		// abuse (secondary) rate limit situation
-		if resp.Header.Get("Retry-After") != "" {
+		if resp.Header.Get("Retry-After") != "" && resp.Header.Get("x-ratelimit-remaining") != "0" {
 			return Secondary
 		}
 
@@ -66,9 +66,8 @@ func (options *RateLimitHandlerOptions) IsRateLimited() func(req *netHttp.Reques
 
 func NewRateLimitHandler() *RateLimitHandler {
 	return NewRateLimitHandlerWithOptions(RateLimitHandlerOptions{
-		ReadDelay:        0 * time.Second,
-		WriteDelay:       1 * time.Second,
-		ParallelRequests: false,
+		ReadDelay:  0 * time.Second,
+		WriteDelay: 1 * time.Second,
 	})
 }
 
@@ -100,12 +99,11 @@ func (handler RateLimitHandler) retryRequest(ctx context.Context, pipeline kiota
 
 	if rateLimitType == Secondary {
 		fmt.Printf("<<<<<<< Abuse detection mechanism triggered, sleeping for %s before retrying\n", resp.Header.Get("Retry-After"))
-		retryAfter := resp.Header.Get("Retry-After")
-		retryAfterDuration, err := time.ParseDuration(retryAfter + "s")
+		retryAfterDuration, err := parseSecondaryRate(resp)
 		if err != nil {
 			return nil, fmt.Errorf("failed to parse retry-after header into duration: %v", err)
 		}
-		time.Sleep(retryAfterDuration)
+		time.Sleep(*retryAfterDuration)
 		return handler.Intercept(pipeline, middlewareIndex, request)
 	}
 
@@ -113,4 +111,36 @@ func (handler RateLimitHandler) retryRequest(ctx context.Context, pipeline kiota
 		fmt.Printf("<<<<<<< Primary rate limit %s reached, sleeping for %s before retrying\n", resp.Header.Get("x-ratelimit-limit"), resp.Header.Get("Retry-After"))
 	}
 	panic("not implemented")
+}
+
+// code stolen from https://github.com/google/go-github/blob/0e3ab5807f0e9bc6ea690f1b49e94b78259f3681/github/github.go#L1096
+// TODO(kfcampbell): validate/give credit/import appropriately if possible
+// parseSecondaryRate parses the secondary rate related headers,
+// and returns the time to retry after.
+func parseSecondaryRate(r *netHttp.Response) (*time.Duration, error) {
+	// According to GitHub support, the "Retry-After" header value will be
+	// an integer which represents the number of seconds that one should
+	// wait before resuming making requests.
+	if v := r.Header.Get("Retry-After"); v != "" {
+		retryAfterSeconds, err := strconv.ParseInt(v, 10, 64) // Error handling is noop.
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse retry-after header into duration: %v", err)
+		}
+		retryAfter := time.Duration(retryAfterSeconds) * time.Second
+		return &retryAfter, nil
+	}
+
+	// According to GitHub support, endpoints might return x-ratelimit-reset instead,
+	// as an integer which represents the number of seconds since epoch UTC,
+	// represting the time to resume making requests.
+	if v := r.Header.Get("X-RateLimit-Reset"); v != "" {
+		secondsSinceEpoch, err := strconv.ParseInt(v, 10, 64) // Error handling is noop.
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse x-ratelimit-reset header into duration: %v", err)
+		}
+		retryAfter := time.Until(time.Unix(secondsSinceEpoch, 0))
+		return &retryAfter, nil
+	}
+
+	return nil, nil
 }
